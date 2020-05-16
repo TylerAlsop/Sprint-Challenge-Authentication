d84eda317941ca4afdcb456cc449aab76b350fb1
Tyler Alsop <tylerjalsop@gmail.com>
2020-05-15 21:28:31 -0600

    Last commit for tonight. Added tests for the /api/login endpoint but they aren't passing eventhough my login endpoints work in insomnia.

--- __tests__/jokes.test.js
+++ __tests__/jokes.test.js
@@ -19,10 +19,10 @@ describe("Jokes integration tests", () => {
 /////////////  Had to comment this out because the tests only work once before needing to be changed /////////////   
 
     // it("POST register endpoint. Valid new user.", async () => {
-    //     const userCred = { username: "user1", password: "password1" }
+    //     const newUserCred = { username: "user1", password: "password1" }
     //     const res = await supertest(server)
     //         .post("/api/auth/register")
-    //         .send(userCred)
+    //         .send(newUserCred)
 
     //     expect(res.statusCode).toBe(201)
 
@@ -31,10 +31,10 @@ describe("Jokes integration tests", () => {
     // it("POST register endpoint. Existing username.", async () => {
     //     const user = await Users.findBy({ username }).first()
 
-    //     const userCred = user
+    //     const newUserCred = user
     //     const res = await supertest(server)
     //         .post("/api/auth/register")
-    //         .send(userCred)
+    //         .send(newUserCred)
 
     //     expect(res.statusCode).toBe(409)
 
@@ -42,6 +42,19 @@ describe("Jokes integration tests", () => {
 
 
 
+    it("POST login endpoint", async () => {
+        const userCred = { username: "Tyler1", password: "Password1" }
+        const res = await supertest(server)
+            .post("/api/auth/login")
+            .send(userCred)
+
+            expect(res.statusCode).toBe(200)
+
+            expect(res.body.username).toBe("Tyler1")
+
+    })
+
+///////////// This is commented out because it requires authorization and we have not be taught how to test with auth. /////////////
 
     // it("GET /api/jokes", async () => {
     //     const res = await supertest(server).get("/api/jokes")
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -56,10 +56,7 @@ router.post('/login', async (req, res, next) => {
 
 		res.cookie("token", jwt.sign(tokenPayload, process.env.JWT_SECRET))
 
-		res.json({
-			message: `Welcome ${user.username}!`,
-			// token: jwt.sign(tokenPayload, process.env.JWT_SECRET),
-    })
+		res.status(200).json(user)
 	} catch(err) {
 		next(err)
 	}
